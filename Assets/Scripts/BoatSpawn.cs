c8ce440672e91954eb0fd4ac4358339804e52c16
Joppe Min <joppemin@hotmail.com>
2019-02-01 22:18:56 +0100

    New Randomization script
    previous boat is never the same

--- Assets/Scripts/BoatSpawn.cs
+++ Assets/Scripts/BoatSpawn.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BoatSpawn : MonoBehaviour
 {
     public GameObject[] BoatObject;
+    int RandomBoat = 0;
 
     Vector3 boatStartPos;
     // Y is neutral and should always stay null or 0
@@ -26,7 +27,14 @@ public class BoatSpawn : MonoBehaviour
     {
         RandomX = Random.Range(-1f, 1f);
         RandomZ = Random.Range(-1f, 1f);
-        int RandomBoat = Random.Range(0, 3);
+
+        //randomization script, previous one is never same
+        RandomBoat += Random.Range(1, 3);
+        if (RandomBoat > 2)
+        {
+            RandomBoat = 0;
+        }
+        
 
         boatStartPos = new Vector3(RandomX, 0, RandomZ);
         transform.position = boatStartPos.normalized * 3f;
